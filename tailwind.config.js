a5e972a94de73e41919a5397b7035e90b7d1f51b
Rahul Saxena <rahulsaxena2021@hotmail.com>
2023-04-17 04:00:36 +0530

    navbar animated button for mobile devices part3 completed version1

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./build/*.{html,js}"],
+  content: ["./build/*.html","./build/js/*.js"],
+  // content: ["./build/*.{html,js}"] is also valid
   theme: {
     extend: {
       screens: {
@@ -8,15 +9,15 @@ module.exports = {
         tallscreen: { raw: "(min-aspect-ratio: 13/20)" },
       },
       keyframes: {
-        'open-menu': {
-          '0%': { transform: 'scaleY(0)' },
-          '80%': { transform: 'scaleY(1.2)' },
-          '100%': { transform: 'scaleY(1)' },
-        }
+        "open-menu": {
+          "0%": { transform: "scaleY(0)" },
+          "80%": { transform: "scaleY(1.2)" },
+          "100%": { transform: "scaleY(1)" },
+        },
       },
       animation: {
-        'open-menu': 'open-menu 0.5s ease-in-out forwards',
-      }
+        "open-menu": "open-menu 0.5s ease-in-out forwards",
+      },
     },
   },
   plugins: [],
